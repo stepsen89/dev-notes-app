39af32d648f37bd4a7e723766cf4ba3b218202f4
[stepsen89] <stephanie.senoner@gmail.com>
2019-10-10 21:04:38 +0100

    post user route including validation

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator/check');
 
 /*
   @route         GET api/users
@@ -7,8 +8,15 @@ const router = express.Router();
   @access        public
 */
 
-router.post('/', (req, res) => {
-  console.log(req.body);
+router.post('/', [
+  check('name', 'name is required').not().isEmpty(),
+  check('email', 'please enter a valid email').isEmail(),
+  check('password', 'please enter a password which is longer than 6 characters').isLength({ min: 6}),
+],(req, res) => {
+  const errors = validationResult(req);
+  if(!errors.isEmpty()){
+    return res.status(400).json({errors: errors.array()})
+  }
   res.send('User route');
 });
 
