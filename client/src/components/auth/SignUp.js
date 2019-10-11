7f3c3cbae23e154a7d9f8ca71588a0a590c6be35
[stepsen89] <stephanie.senoner@gmail.com>
2019-10-11 21:13:19 +0100

    implementing redux state management store

--- client/src/components/auth/SignUp.js
+++ client/src/components/auth/SignUp.js
@@ -1,4 +1,5 @@
 import React, { Fragment, useState } from "react";
+import { Link } from "react-router-dom";
 import axios from "axios";
 
 const SignUp = () => {
@@ -100,7 +101,7 @@ const SignUp = () => {
         <input type="submit" className="btn btn-primary" value="Register" />
       </form>
       <p className="my-1">
-        Already have an account? <a href="login.html">Sign In</a>
+        Already have an account? <Link to="/login">Log In</Link>
       </p>
     </Fragment>
   );
