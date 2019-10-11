fcdf50f087fb81a0c4064a4cdbad8909f4f71b52
[stepsen89] <stephanie.senoner@gmail.com>
2019-10-11 17:35:59 +0100

    added route for signup and login

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,24 @@
 import React, { Fragment } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Navbar from "./components/layout/Navbar";
+import Landing from "./components/layout/Landing";
+import SignUp from "./components/auth/SignUp";
+import LogIn from "./components/auth/LogIn";
 import "./App.css";
 
 const App = () => (
-  <Fragment>
-    <h1> App </h1>
-  </Fragment>
+  <Router>
+    <Fragment>
+      <Navbar />
+      <Route exact path="/" component={Landing} />
+      <section className="container">
+        <Switch>
+          <Route exact path="/signup" component={SignUp} />
+          <Route exact path="/login" component={LogIn} />
+        </Switch>
+      </section>
+    </Fragment>
+  </Router>
 );
 
 export default App;
