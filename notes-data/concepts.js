8485c6dbfffdd12ef73ad5d320eb4fa5808e4026
[stepsen89] <stephanie.senoner@gmail.com>
2019-10-13 18:04:32 +0200

    added notes for javascript types

---
+++ notes-data/concepts.js
@@ -0,0 +1,10 @@
+"use strict";
+var a = 1;
+var b = {};
+function foo(x, y) {
+  x = 2;
+  y.moo = 3;
+}
+
+foo(a, b);
+console.log("a = " + a + "; b = " + JSON.stringify(b));
---
+++ notes-data/types.js
@@ -0,0 +1,30 @@
+// what are different types of javascript?
+
+// primitive: boolean, number, string, null, undefined
+// non primitive: object
+
+// typeof returns the typeof
+
+// dynamically typed
+the type of var a = 'foo' will be infered
+// A language is dynamically-typed if the type of a variable is checked during run-time
+
+// static typed like JAVA you have to specify what type each variable will hold
+String a = 'moo'
+
+// advantages of dynamically typed languages
+
+// advantages of statical typed languages
+
+// if JavaScrtipt doesn't know the type of a variable it will be initialised with undefined
+
+// it is used to inform you that it is either an unitialised value, a parameter that is missing from the
+// parameters list or it is perhabs an unknown property
+// null can set the value of a variable to null, JavaScript engine will never do that for you
+// in Java it is the concept of an absent value
+
+// type of null will return object but this is a mistake of the JavaScript engine known for years now
+
+// null == undefined will return true (the value)
+
+
