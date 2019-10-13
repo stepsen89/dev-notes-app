a8c7eefc69521615430fa750b0747aec3b9d01e3
[stepsen89] <stephanie.senoner@gmail.com>
2019-10-13 18:21:55 +0200

    coercion

--- notes-data/concepts.js
+++ notes-data/concepts.js
@@ -8,3 +8,17 @@ function foo(x, y) {
 
 foo(a, b);
 console.log("a = " + a + "; b = " + JSON.stringify(b));
+
+// ----------------------------------------------------
+
+// throwing error in use strict mode but not in non use strict mode
+
+// What will the below code print out?
+("use strict");
+var myVariable = 3;
+myVarible = 1; // spelling mistake
+console.log(myVarible);
+
+// will throw an error
+
+// ----------------------------------------------------
