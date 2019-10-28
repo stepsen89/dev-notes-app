b8c84a4ab5bc2ae1f786d50118053ba309f5c65c
[stepsen89] <stephanie.senoner@gmail.com>
2019-10-28 19:47:13 +0000

    getting all profiles

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -102,4 +102,14 @@ router.post(
   }
 );
 
+router.get("/", async (req, res) => {
+  try {
+    const profiles = await Profile.find().populate("user", ["name", "avatar"]);
+    res.json(profiles);
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).send("Server Error");
+  }
+});
+
 module.exports = router;
