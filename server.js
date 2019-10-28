d49855bfcb64f236ae0f5d02ea811e33237dcf2b
[stepsen89] <stephanie.senoner@gmail.com>
2019-10-28 23:07:05 +0000

    added posts model and route set up for posts

--- server.js
+++ server.js
@@ -15,8 +15,8 @@ app.get("/", (req, res) => res.send("API running"));
 app.use("/api/users", require("./routes/api/users"));
 app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/posts", require("./routes/api/posts"));
 
-// app.use('/api/posts', require('./routes/api/posts'));
 // app.use('/api/notes', require('./routes/api/notes'));
 
 const PORT = process.env.PORT || 5000;
